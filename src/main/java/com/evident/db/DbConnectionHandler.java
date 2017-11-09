4ada6557c6bc44bab3604d0a29dfedb1609d2e0d
medonyo <yahonda@tutanota.com>
2017-11-09 21:51:18 +0100

    Some cleaning up.

--- src/main/java/com/evident/db/DbConnectionHandler.java
+++ src/main/java/com/evident/db/DbConnectionHandler.java
@@ -17,73 +17,80 @@ import com.main.EvidentApp.ClientData;
 // TODO: add tests for this class
 public class DbConnectionHandler {
 
-    private static SessionFactory sessionFactory = null;  
-    private static ServiceRegistry serviceRegistry = null;  
+    private static SessionFactory sessionFactory = null;
+    private static ServiceRegistry serviceRegistry = null;
     private static Session session = null;
-    private static Transaction tx=null;
-       
-    public static SessionFactory configureSessionFactory() throws HibernateException {  	
+    private static Transaction tx = null;
+
+    public static SessionFactory configureSessionFactory() throws HibernateException {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().
-            		configure("hibernate.cfg.xml").build();
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate.cfg.xml").build();
             Metadata metadata = new MetadataSources(standardRegistry).getMetadataBuilder().build();
             sessionFactory = metadata.getSessionFactoryBuilder().build();
             return sessionFactory;
-        } 
-        catch (Throwable ex) {
+        } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-     
-    public static void saveClientData(ClientData data){
-    	// TODO:
-    	// Do other actions before saving
-    	
-    	try {
-    		session = sessionFactory.openSession();
-    		tx = session.beginTransaction();
+
+    public static void saveClientData(ClientData data) {
+        // TODO:
+        // Do other actions before saving
+
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
             // Saving to the database
             session.save(data);
             // Committing the change in the database.
             session.flush();
             tx.commit();
-    		
+
         } catch (Exception ex) {
             ex.printStackTrace();
-             
-            // Rolling back the changes to make the data consistent in case of any failure 
+
+            // Rolling back the changes to make the data consistent in case of
+            // any failure
             // in between multiple database write operations.
             tx.rollback();
-        } finally{
-            if(session != null) {
+        } finally {
+            if (session != null) {
                 session.close();
             }
         }
     }
-    
-    public static void displayAll(){
+
+    public static String getUser() {
+        // TODO: this is only stub for getting user. Need to be changed.
+        return "Super person super";
+    }
+
+    public static void displayAll() {
         try {
             session = sessionFactory.openSession();
-             
+
             // Fetching saved data
             List<ClientData> clientsLinst = session.createQuery("from ClientData").list();
-             
+
             for (ClientData client : clientsLinst) {
-                System.out.println("Id: " + client.getId() + " | Name:"  + client.getName() + " | Email:" + client.getEmail());
+                System.out.println("Id: " + client.getId() + " | Name:" + client.getName()
+                        + " | Email:" + client.getEmail());
             }
         } catch (Exception ex) {
             ex.printStackTrace();
-             
-            // Rolling back the changes to make the data consistent in case of any failure 
+
+            // Rolling back the changes to make the data consistent in case of
+            // any failure
             // in between multiple database write operations.
             tx.rollback();
-        } finally{
-            if(session != null) {
+        } finally {
+            if (session != null) {
                 session.close();
             }
-        } 
+        }
     }
 }
--- src/main/java/com/main/EvidentApp/App.java
+++ src/main/java/com/main/EvidentApp/App.java
@@ -1,68 +1,53 @@
 package com.main.EvidentApp;
 
 import java.io.IOException;
-import java.sql.Date;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import com.evident.db.DbConnectionHandler;
 
 //import com.evident.db.DbConnectionHandler;
 
-public class App extends HttpServlet { 
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class App extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//resp.getWriter().println("TESTING_TESTING___......");
-		//resp.getWriter().println(new Date(0));
-		req.setAttribute("client_name", "client_got_from_DB");
-		
-		ServletContext context = getServletContext();		
-		RequestDispatcher rd = context.getRequestDispatcher("/WEB-INF/views/client_view.jsp");
-		rd.forward(req, resp);
-	}
-/*	
-	private static DbConnectionHandler connection;
-    
-    public static void main(String[] args) {
-        // Configure the session factory
-    	connection.configureSessionFactory();
-           
-		// Creating Contact entity that will be save to the sqlite database
-		ClientData client1 = new ClientData(1, "Name 7", "Surname3",100,"555444333","sharing3@email.com");
-		ClientData client2 = new ClientData(2, "Name 884", "Surname44",144,"333222111","telling44@email.com");
-		 
-		
-		connection.saveClientData(client1);
-		connection.saveClientData(client2);
-		
-		
-		connection.displayAll();
-            
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+
+        DbConnectionHandler connection = null;
+        req.setAttribute("client_name", connection.getUser());
+
+        ServletContext context = getServletContext();
+        RequestDispatcher rd = context.getRequestDispatcher("/WEB-INF/views/client_view.jsp");
+        rd.forward(req, resp);
     }
-*/
+
+    private static void ExecuteTransaction() {
+
+        DbConnectionHandler connection = null;
+
+        // Configure the session factory
+        connection.configureSessionFactory();
+
+        // Creating Contact entity that will be save to the sqlite database
+        ClientData client1 = new ClientData(1, "Name 7", "Surname3", 100, "555444333",
+                "sharing3@email.com");
+        ClientData client2 = new ClientData(2, "Name 884", "Surname44", 144, "333222111",
+                "telling44@email.com");
+
+        connection.saveClientData(client1);
+        connection.saveClientData(client2);
+
+        connection.displayAll();
+    }
 }
